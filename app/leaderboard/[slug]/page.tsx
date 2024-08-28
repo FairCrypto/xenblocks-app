df63f1dc0908b9a8fb985532f7c6e037ea59003e
Nicholas Pettas <npettas@gmail.com>
2024-08-28 16:50:17 -0700

    improve the account not found alert

--- app/leaderboard/[slug]/page.tsx
+++ app/leaderboard/[slug]/page.tsx
@@ -49,7 +49,24 @@ export default function LeaderboardSlug({
               </div>
             </div>
 
-            {error && <div className="alert alert-warning">{error}</div>}
+            {error && (
+              <div role="alert" className="alert alert-warning rounded my-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
+                </svg>
+                <span className="text-xs sm:text-base">{error}</span>
+              </div>
+            )}
 
             <div>
               <p className="my-3 text-xs sm:text-base">
